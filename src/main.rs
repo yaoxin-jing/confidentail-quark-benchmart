d4f1b043db9c154cb06a3ce16fa35fd6b358b693
yaoxin1 <yaoxin.jing1@huawei.com>
2023-05-03 18:23:27 +0200

    add code for application launch test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::core::v1::Pod;
 use tracing::*;
-use anyhow::{Context, Ok};
+use anyhow::{Context, Result};
 use kube::{
     api::{
         Api, DeleteParams, PostParams, ResourceExt, WatchEvent, WatchParams},
@@ -10,23 +10,6 @@ use kube::{
 };
 use tokio::process::Command;
 
-
-// pub fn time_before() -> i64 {
-//     let rax: u64;
-//     let rdx: u64;
-
-//     unsafe {
-//         std::arch::asm!("
-//             lfence
-//             rdtsc
-//             ",
-//             out("rax") rax,
-//             out("rdx") rdx
-//         )
-//     };
-
-//     return rax as i64 | ((rdx as i64) << 32);
-// }
 
 async fn test_exec_cmd(loop_times: i32, pod_name: String, file_path: std::path::PathBuf) ->  anyhow::Result<()>{
 
@@ -46,6 +29,66 @@ async fn test_exec_cmd(loop_times: i32, pod_name: String, file_path: std::path::
 
 
 
+async fn is_pod_ready(pod_name: &str) -> bool {
+
+    let mut child = Command::new("bash")
+    .args([
+        "-c",
+        "kubectl get pod"
+    ])
+    .output()
+    .await.expect("failed to get pod");
+
+    let stdout = child.stdout;
+    let s: std::borrow::Cow<str> = String::from_utf8_lossy(&stdout);
+    
+    let mut target_line = Vec::new();
+    let mut found: bool = false;
+    for line in s.lines() {
+        let vec_str: Vec<&str> = line.split_whitespace().collect();
+        if vec_str[0].eq(pod_name) {
+            target_line = vec_str;
+            found = true;
+        }
+    }
+
+    if !found {
+        return false;
+    }
+
+    println!("{:?}", target_line);
+    let status = target_line[2];
+    match status {
+        "Running" => {
+            info!("pod is running");
+            return true;
+        }
+        _ => {
+            info!("pedding {}", status);
+            return false
+        },
+        
+    }
+}
+
+
+async fn clean_up() -> Result<()> {
+    let cmd = format!("sudo pkill -9 containerd-shim");
+
+    let mut child = Command::new("bash")
+    .args([
+        "-c",
+        &cmd
+    ])
+    .output()
+    .await.expect("failed to delete pod");
+
+    info!("{:?}", child);
+
+    Ok(())
+}
+
+
 async fn test_app_lauch_time(loop_times: i32, pod_name: String, file_path: std::path::PathBuf) ->  anyhow::Result<i32>{
 
     let client = Client::try_default().await?;
@@ -53,62 +96,52 @@ async fn test_app_lauch_time(loop_times: i32, pod_name: String, file_path: std::
     let p: Pod = serde_yaml::from_str(&yaml)?;
     let pods: Api<Pod> = Api::default_namespaced(client);
     let fileds_selector = format!("metadata.name={}", pod_name);
-
     let mut i = 0;
 
-    let delete_params = DeleteParams::default().grace_period(0);
-    // Delete it
-    // pods.delete(&pod_name, &delete_params)
-    //     .await?
-    //     .map_right(|pdel| {
-    //     });
+    let delete_pod = format!("kubectl delete pod {} --grace-period=0 --force", pod_name);
+
+    let mut child = Command::new("bash")
+    .args([
+        "-c",
+        &delete_pod
+    ])
+    .output()
+    .await.expect("failed to delete pod");
 
     while i < loop_times {
         info!("loop {}, pod_name {:?}", i, pod_name);
-        pods.create(&PostParams::default(), &p).await?;
-        // Wait until the pod is running, otherwise we get 500 error.
-        let wp = WatchParams::default().fields(&fileds_selector).timeout(10);
-        let mut stream = pods.watch(&wp, "0").await?.boxed();
-        while let Some(status) = stream.try_next().await? {
-            match status {
-                WatchEvent::Added(o) => {
-                    info!("Added {}", o.name_any());
-                }
-                WatchEvent::Modified(o) => {
-                    let s = o.status.as_ref().expect("status exists on pod");
-                    if s.phase.clone().unwrap_or_default() == "Running" {
-                        info!("Pod is running {}", o.name_any());
-                        break;
-                    }
-                }
-                _ => {}
-            }
-        }
+        // pods.create(&PostParams::default(), &p).await?;
 
-        // tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-        let delete_params = DeleteParams::default().grace_period(0);
-        // Delete it
-        pods.delete(&pod_name, &delete_params)
-            .await?
-            .map_right(|pdel| {
-            });
+        let mut child = Command::new("bash")
+        .args([
+            "-c",
+            "kubectl apply -f /home/yaoxin/demo/benchmark/mongodb.yaml"
+        ])
+        .output()
+        .await.expect("failed to execute process");
+
+
         
-        let wp = WatchParams::default().fields(&fileds_selector).timeout(10);
-        let mut stream = pods.watch(&wp, "0").await?.boxed();
-        while let Some(status) = stream.try_next().await? {
-            match status {
-                WatchEvent::Deleted(o) => {
-                    let s = o.status.as_ref().expect("status exists on pod");
-                    if s.phase.clone().unwrap_or_default() == "Running" {
-                        info!("loop {} pod {} deleted", i, o.name_any());
-                        break;
-                    }
-                }
-                _ => {}
-            }
-        }
+        while !(is_pod_ready(&pod_name).await) {}
+        
+
+        let delete_pod = format!("kubectl delete pod {} --grace-period=0 --force", pod_name);
+
+        let mut child = Command::new("bash")
+        .args([
+            "-c",
+            &delete_pod
+        ])
+        .output()
+        .await.expect("failed to delete pod");
+
+        info!("{:?}", child);
+
+        
+
         i = i + 1;
     }
+    // clean_up().await.unwrap();
 
     Ok(i)    
 }
@@ -118,16 +151,10 @@ async fn test_app_lauch_time(loop_times: i32, pod_name: String, file_path: std::
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-<<<<<<< HEAD
     let mongo_path = std::path::PathBuf::from("/home/yaoxin/demo/benchmark/mongodb.yaml");
     let nginx_path = std::path::PathBuf::from("/home/yaoxin/demo/benchmark/nginx.yaml");
-    let res = test_app_lauch_time(150, "quark-pod-mongo".to_string(), mongo_path).await?;
+    let res = test_app_lauch_time(150, "quark-pod-mongo1".to_string(), mongo_path).await?;
     assert!(res == 150);
-=======
-    let path = std::path::PathBuf::from("/home/yaoxin/test/mongo.yaml");
-    let res = test_app_lauch_time(10, "quark-pod-mongo".to_string(), path).await?;
-    assert!(res == 10);
->>>>>>> 7a260489b560675bd9301c97cdd56a8d2768955c
 
     println!("test finished {}", res);
     Ok(())
